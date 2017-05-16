ebe7fa4a1dad8e712abbe9cdac9a6e0fca4bb230
Siripong <ibabenz.littledreamer@gmail.com>
2017-05-16 11:24:10 +0700

    Intent to Register

--- app/src/main/java/fetl/kachintorn/siripong/fetlqrcode/MainActivity.java
+++ app/src/main/java/fetl/kachintorn/siripong/fetlqrcode/MainActivity.java
@@ -1,13 +1,61 @@
 package fetl.kachintorn.siripong.fetlqrcode;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    //Explicit
+    private EditText userEditText, passwordEditText;
+    private TextView textView;
+    private Button button;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        //Initial View
+        initialView();
+
+        // Controller
+        controller();
+
+    }   // Main Method
+
+    private void controller() {
+        textView.setOnClickListener(MainActivity.this);
+        button.setOnClickListener(MainActivity.this);
+
     }
-}
+
+    private void initialView() {
+        userEditText = (EditText) findViewById(R.id.edtUser);
+        passwordEditText = (EditText) findViewById(R.id.edtPassword);
+        textView = (TextView) findViewById(R.id.txtNewRegis);
+        button = (Button) findViewById(R.id.btnLogin);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+            // For TextView
+        if (v==textView) {
+
+                //Intent to Register
+            Intent intent = new Intent(MainActivity.this,RegisterActivity.class);
+                    startActivity(intent);
+                //
+            if (v==button) {
+
+            }
+        }
+    }
+}   //Main Class คลาสหลัก
