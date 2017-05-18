0ec2f538cd295ac7e61b3c1d7f7940dfc2bd7e23
Siripong <ibabenz.littledreamer@gmail.com>
2017-05-18 09:45:33 +0700

    Show Text OK on 18/05/17

--- app/src/main/java/fetl/kachintorn/siripong/fetlqrcode/ServiceActivity.java
+++ app/src/main/java/fetl/kachintorn/siripong/fetlqrcode/ServiceActivity.java
@@ -2,12 +2,39 @@ package fetl.kachintorn.siripong.fetlqrcode;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telecom.TelecomManager;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class ServiceActivity extends AppCompatActivity {
+
+    //  Explicit
+    private TextView textView;
+    private ImageView barImageView, qrImageView;
+    private ListView listView;
+    private String nameString;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_service);
+
+
+        //Initial View
+        initialView();
+
+        //Show Text
+        nameString = getIntent().getStringExtra("Login");
+            textView.setText("สวัสดี " + nameString);
+
+
     }   //Main Method
+
+    private void initialView() {
+        textView = (TextView) findViewById(R.id.txtNameLogin);
+        barImageView = (ImageView) findViewById(R.id.imvBarCode);
+        qrImageView = (ImageView) findViewById(R.id.imvBarCode);
+        listView = (ListView) findViewById(R.id.livProduct);
+    }
 }   //Main Class
